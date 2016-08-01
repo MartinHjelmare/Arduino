22c5138d5da3d02295aaf7f52aa871b0a10c436d
MartinHjelmare <marhje52@kth.se>
2016-08-01 02:16:40 +0200

    Add sketch for relay with button

---
+++ examples/TestRelayButton/TestRelayButton.ino
@@ -0,0 +1,100 @@
+/*
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * http://www.mysensors.org/build/relay
+ */
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+// Enabled repeater feature for this node
+#define MY_REPEATER_FEATURE
+#define MY_NODE_ID 1
+#include <SPI.h>
+#include <MySensors.h>
+#include <Bounce2.h>
+
+#define RELAY_PIN  5  // Arduino Digital I/O pin number for relay
+#define BUTTON_PIN  3  // Arduino Digital I/O pin number for button
+#define CHILD_ID 1
+#define RELAY_ON 1
+#define RELAY_OFF 0
+
+Bounce debouncer = Bounce();
+bool state = false;
+bool initialValueSent = false;
+
+MyMessage msg(CHILD_ID, V_STATUS);
+
+void setup()
+{
+  // Setup the button
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // After setting up the button, setup debouncer
+  debouncer.attach(BUTTON_PIN);
+  debouncer.interval(10);
+
+  // Make sure relays are off when starting up
+  digitalWrite(RELAY_PIN, RELAY_OFF);
+  // Then set relay pins in output mode
+  pinMode(RELAY_PIN, OUTPUT);
+
+  // Set relay to last known state (using eeprom storage)
+  state = loadState(CHILD_ID);
+  digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
+}
+
+void presentation()  {
+  sendSketchInfo("Lamp", "1.0");
+
+  present(CHILD_ID, S_BINARY);
+}
+
+void loop()
+{
+  if (!initialValueSent) {
+    Serial.println("Sending initial value");
+    send(msg.set(state?RELAY_ON:RELAY_OFF));
+    Serial.println("Requesting initial value from controller");
+    request(CHILD_ID, V_STATUS);
+    wait(2000, C_SET, V_STATUS);
+  }
+  if (debouncer.update()) {
+    if (debouncer.read()==LOW) {
+      state = !state;
+      send(msg.set(state?RELAY_ON:RELAY_OFF), true); // Send new state and request ack back
+    }
+  }
+}
+
+void receive(const MyMessage &message) {
+  if (message.isAck()) {
+     Serial.println("This is an ack from gateway");
+  }
+
+  if (message.type == V_STATUS) {
+    if (!initialValueSent) {
+      Serial.println("Receiving initial value from controller");
+      initialValueSent = true;
+    }
+    // Change relay state
+
+    state = (bool)message.getInt();
+    digitalWrite(RELAY_PIN, state?RELAY_ON:RELAY_OFF);
+    send(msg.set(state?RELAY_ON:RELAY_OFF));
+    // Store state in eeprom
+    saveState(CHILD_ID, state);
+
+    // Write some debug info
+    Serial.print("Incoming change for sensor:");
+    Serial.print(message.sensor);
+    Serial.print(", New status: ");
+    Serial.println(message.getInt());
+  }
+}
