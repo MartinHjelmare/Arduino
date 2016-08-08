67eb3a16cfa14900da403a3945b723b3ede67e22
MartinHjelmare <marhje52@kth.se>
2016-08-08 19:26:41 +0200

    Add Sensebender sketches and test channels

--- examples/TestRGBDimmer/TestRGBDimmer.ino
+++ examples/TestRGBDimmer/TestRGBDimmer.ino
@@ -11,6 +11,7 @@
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
+#define MY_RF24_CHANNEL	78
 
 // Enable repeater functionality for this node
 #define MY_REPEATER_FEATURE
