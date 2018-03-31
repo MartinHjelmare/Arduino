93b162ef9dfa0ab28d01ee1dedbd203d2a7dafa5
Martin Hjelmare <marhje52@kth.se>
2018-04-01 00:18:42 +0200

    Update test mqtt client sketch

--- examples/TestGatewayW5100MQTTClient/TestGatewayW5100MQTTClient.ino
+++ examples/TestGatewayW5100MQTTClient/TestGatewayW5100MQTTClient.ino
@@ -49,7 +49,7 @@
  * - Connect GPIO2 via 10K resistor to VCC
  * - Connect GPIO0 via 10K resistor to VCC, and via switch to GND ('bootload switch')
  *
- * Inclusion mode button:
+  * Inclusion mode button:
  * - Connect GPIO5 via switch to GND ('inclusion switch')
  *
  * Hardware SHA204 signing is currently not supported!
@@ -57,7 +57,6 @@
  * Make sure to fill in your ssid and WiFi password below for ssid & pass.
  */
 
-#include <SPI.h>
 
 // Enable debug prints to serial monitor
 #define MY_DEBUG
@@ -65,11 +64,13 @@
 // Enables and select radio type (if attached)
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
-#define MY_RF24_CHANNEL	78
+//#define MY_RADIO_RFM95
+
+#define MY_RF24_CHANNEL	82
 
 #define MY_GATEWAY_MQTT_CLIENT
 
-// Set this nodes subscripe and publish topic prefix
+// Set this node's subscribe and publish topic prefix
 #define MY_MQTT_PUBLISH_TOPIC_PREFIX "mysensors/mygateway1-out"
 #define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX "mysensors/mygateway1-in"
 
@@ -83,28 +84,28 @@
 // The W5100 ethernet module seems to have a hard time co-operate with
 // radio on the same spi bus.
 #if !defined(MY_W5100_SPI_EN) && !defined(ARDUINO_ARCH_SAMD)
-  #define MY_SOFTSPI
-  #define MY_SOFT_SPI_SCK_PIN 14
-  #define MY_SOFT_SPI_MISO_PIN 16
-  #define MY_SOFT_SPI_MOSI_PIN 15
+#define MY_SOFTSPI
+#define MY_SOFT_SPI_SCK_PIN 14
+#define MY_SOFT_SPI_MISO_PIN 16
+#define MY_SOFT_SPI_MOSI_PIN 15
 #endif
 
 // When W5100 is connected we have to move CE/CSN pins for NRF radio
 #ifndef MY_RF24_CE_PIN
-  #define MY_RF24_CE_PIN 5
+#define MY_RF24_CE_PIN 5
 #endif
 #ifndef MY_RF24_CS_PIN
-  #define MY_RF24_CS_PIN 6
+#define MY_RF24_CS_PIN 6
 #endif
 
-// Enable these if your MQTT broker requires usenrame/password
+// Enable these if your MQTT broker requires username/password
 #define MY_MQTT_USER "mysensors"
 #define MY_MQTT_PASSWORD "PASSWORD"
 
 // Enable MY_IP_ADDRESS here if you want a static ip address (no DHCP)
 //#define MY_IP_ADDRESS 192,168,178,87
 
-// If using static ip you need to define Gateway and Subnet address as well
+// If using static ip you can define Gateway and Subnet address as well
 //#define MY_IP_GATEWAY_ADDRESS 192,168,178,1
 //#define MY_IP_SUBNET_ADDRESS 255,255,255,0
 
@@ -116,37 +117,39 @@
 #define MY_PORT 1883
 
 /*
-   // Flash leds on rx/tx/err
-   #define MY_LEDS_BLINKING_FEATURE
-   // Set blinking period
-   #define MY_DEFAULT_LED_BLINK_PERIOD 300
+// Enable inclusion mode
+#define MY_INCLUSION_MODE_FEATURE
+// Enable Inclusion mode button on gateway
+//#define MY_INCLUSION_BUTTON_FEATURE
+// Set inclusion mode duration (in seconds)
+#define MY_INCLUSION_MODE_DURATION 60
+// Digital pin used for inclusion mode button
+//#define MY_INCLUSION_MODE_BUTTON_PIN  3
 
-   // Enable inclusion mode
-   #define MY_INCLUSION_MODE_FEATURE
-   // Enable Inclusion mode button on gateway
-   #define MY_INCLUSION_BUTTON_FEATURE
-   // Set inclusion mode duration (in seconds)
-   #define MY_INCLUSION_MODE_DURATION 60
-   // Digital pin used for inclusion mode button
-   #define MY_INCLUSION_MODE_BUTTON_PIN  3
+// Set blinking period
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
 
-   // Uncomment to override default HW configurations
-   //#define MY_DEFAULT_ERR_LED_PIN 16  // Error led pin
-   //#define MY_DEFAULT_RX_LED_PIN  16  // Receive led pin
-   //#define MY_DEFAULT_TX_LED_PIN  16  // the PCB, on board LED
- */
+// Flash leds on rx/tx/err
+// Uncomment to override default HW configurations
+//#define MY_DEFAULT_ERR_LED_PIN 16  // Error led pin
+//#define MY_DEFAULT_RX_LED_PIN  16  // Receive led pin
+//#define MY_DEFAULT_TX_LED_PIN  16  // the PCB, on board LED
+*/
 
 #include <Ethernet.h>
 #include <MySensors.h>
 
-void setup() {
+void setup()
+{
+	// Setup locally attached sensors
 }
 
-void presentation() {
-        // Present locally attached sensors here
+void presentation()
+{
+	// Present locally attached sensors here
 }
 
-
-void loop() {
-        // Send locally attech sensors data here
+void loop()
+{
+	// Send locally attached sensors data here
 }
