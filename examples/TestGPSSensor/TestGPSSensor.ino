b0ddda7b2e312ec16412cd3ab680e39d0c43e714
Martin Hjelmare <marhje52@gmail.com>
2021-03-06 22:58:55 +0100

    Test GPS sensor

--- examples/TestGPSSensor/TestGPSSensor.ino
+++ examples/TestGPSSensor/TestGPSSensor.ino
@@ -10,14 +10,14 @@
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
-#define MY_RF24_CHANNEL	78
+#define MY_RF24_CHANNEL 80
 
 #include <MySensors.h>
 
 #define SN "GPS Sensor"
 #define SV "1.0"
 
-// GPS position send interval (in millisectonds)
+// GPS position send interval (in milliseconds)
 #define GPS_SEND_INTERVAL 30000
 // The child id used for the gps sensor
 #define CHILD_ID_GPS 1
@@ -38,34 +38,36 @@ float gpsLocationLat = 40.741895;
 float gpsLocationLng = -73.989308;
 float gpsAltitudeMeters = 12.0;
 
-void setup() {
-
+void setup()
+{
 }
 
-void presentation() {
-  sendSketchInfo(SN, SV);
-  present(CHILD_ID_GPS, S_GPS);
+void presentation()
+{
+	sendSketchInfo(SN, SV);
+	present(CHILD_ID_GPS, S_GPS);
 }
 
 void loop()
 {
-  unsigned long currentTime = millis();
+	unsigned long currentTime = millis();
 
-  // Evaluate if it is time to send a new position
-  bool timeToSend = currentTime - lastGPSSent > GPS_SEND_INTERVAL;
+	// Evaluate if it is time to send a new position
+	bool timeToSend = currentTime - lastGPSSent > GPS_SEND_INTERVAL;
 
-  if (timeToSend) {
-    // Send current gps location
-    // Build position and altitude string to send
-    dtostrf(gpsLocationLat, 1, 6, latBuf);
-    dtostrf(gpsLocationLng, 1, 6, lngBuf);
-    dtostrf(gpsAltitudeMeters, 1, 0, altBuf);
-    sprintf(payload, "%s,%s,%s", latBuf, lngBuf, altBuf);
+	if (timeToSend)
+	{
+		// Send current gps location
+		// Build position and altitude string to send
+		dtostrf(gpsLocationLat, 1, 6, latBuf);
+		dtostrf(gpsLocationLng, 1, 6, lngBuf);
+		dtostrf(gpsAltitudeMeters, 1, 0, altBuf);
+		sprintf(payload, "%s,%s,%s", latBuf, lngBuf, altBuf);
 
-    Serial.print(F("Position: "));
-    Serial.println(payload);
+		Serial.print(F("Position: "));
+		Serial.println(payload);
 
-    send(msg.set(payload));
-    lastGPSSent = currentTime;
-  }
+		send(msg.set(payload));
+		lastGPSSent = currentTime;
+	}
 }
