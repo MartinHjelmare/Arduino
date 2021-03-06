62ede10c1788baf795eb8ae6e7182914ec8fb694
Martin Hjelmare <marhje52@gmail.com>
2021-03-06 16:51:57 +0100

    Test text sensor

--- examples/TestTextSensor/TestTextSensor.ino
+++ examples/TestTextSensor/TestTextSensor.ino
@@ -4,7 +4,7 @@
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
-#define MY_RF24_CHANNEL	78
+#define MY_RF24_CHANNEL 80
 // Enable repeater functionality for this node
 #define MY_REPEATER_FEATURE
 
@@ -18,37 +18,44 @@
 MyMessage textMsg(CHILD_ID, V_TEXT);
 bool initialValueSent = false;
 
-void setup(void) {
+void setup(void)
+{
 }
 
-void presentation() {
-  sendSketchInfo(SN, SV);
-  present(CHILD_ID, S_INFO, "TextSensor1");
+void presentation()
+{
+	sendSketchInfo(SN, SV);
+	present(CHILD_ID, S_INFO, "TextSensor1");
 }
 
-void loop() {
-  if (!initialValueSent) {
-    Serial.println("Sending initial value");
-    // Send initial values.
-    send(textMsg.set("-"));
-    Serial.println("Requesting initial value from controller");
-    request(CHILD_ID, V_TEXT);
-    wait(2000, C_SET, V_TEXT);
-  }
+void loop()
+{
+	if (!initialValueSent)
+	{
+		Serial.println("Sending initial value");
+		// Send initial values.
+		send(textMsg.set("-"));
+		Serial.println("Requesting initial value from controller");
+		request(CHILD_ID, V_TEXT);
+		wait(2000, C_SET, V_TEXT);
+	}
 }
 
-void receive(const MyMessage &message) {
-  if (message.type == V_TEXT) {
-    if (!initialValueSent) {
-      Serial.println("Receiving initial value from controller");
-      initialValueSent = true;
-    }
-    // Dummy print
-    Serial.print("Message: ");
-    Serial.print(message.sensor);
-    Serial.print(", Message: ");
-    Serial.println(message.getString());
-    // Send message to controller
-    send(textMsg.set(message.getString()));
-  }
+void receive(const MyMessage &message)
+{
+	if (message.type == V_TEXT)
+	{
+		if (!initialValueSent)
+		{
+			Serial.println("Receiving initial value from controller");
+			initialValueSent = true;
+		}
+		// Dummy print
+		Serial.print("Message: ");
+		Serial.print(message.sensor);
+		Serial.print(", Message: ");
+		Serial.println(message.getString());
+		// Send message to controller
+		send(textMsg.set(message.getString()));
+	}
 }
